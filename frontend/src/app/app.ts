806a693b1717b6e56d8a74d5473c7fbab711c0e8
Elisabete Barbosa <ecrbarbosa7@gmail.com>
2026-05-05 22:37:24 +0200

    base project setup

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,10 +1,8 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.html',
+  templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class App {
