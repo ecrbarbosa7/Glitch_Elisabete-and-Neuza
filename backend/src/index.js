a899aaa27df8b91e5a3c3497c29a201634d5055e
Elisabete Barbosa <ecrbarbosa7@gmail.com>
2026-05-06 15:20:17 +0200

    fix gitignore and project cleanup

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,20 +1,18 @@
 import express from 'express';
 import cors from 'cors';
+import authRoutes from './routes/auth.routes.js';
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-// rotas
-import authRoutes from './routes/auth.routes.js';
+app.get('/', (req, res) => {
+  res.send('API Glitch a funcionar');
+});
+
 app.use('/api/auth', authRoutes);
 
 app.listen(3000, () => {
-  console.log('Servidor a correr');
-});  
-
-app.get('/', (req, res) => {
-  res.send('API Glitch a funcionar');
-
+  console.log('Servidor a correr na porta 3000');
 });
