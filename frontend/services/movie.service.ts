113aa4864a218b9355b3e1fd3796d0f8c82df8b0
neuza <neuzap963@gmail.com>
2026-05-19 18:01:31 +0200

    Fix JWT authorization headers in Angular services

--- frontend/services/movie.service.ts
+++ frontend/services/movie.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
 @Injectable({
@@ -10,19 +10,44 @@ export class MovieService {
 
   constructor(private http: HttpClient) {}
 
+  private getAuthHeaders(): HttpHeaders {
+    const token = localStorage.getItem('token');
+
+    return new HttpHeaders({
+      Authorization: `Bearer ${token}`
+    });
+  }
+
   getMovies(): Observable<any[]> {
     return this.http.get<any[]>(this.apiUrl);
   }
 
   createMovie(movie: any): Observable<any> {
-    return this.http.post<any>(this.apiUrl, movie);
+    return this.http.post<any>(
+      this.apiUrl,
+      movie,
+      {
+        headers: this.getAuthHeaders()
+      }
+    );
   }
 
   updateMovie(id: string, movie: any): Observable<any> {
-    return this.http.put<any>(`${this.apiUrl}/${id}`, movie);
+    return this.http.put<any>(
+      `${this.apiUrl}/${id}`,
+      movie,
+      {
+        headers: this.getAuthHeaders()
+      }
+    );
   }
 
   deleteMovie(id: string): Observable<any> {
-    return this.http.delete<any>(`${this.apiUrl}/${id}`);
+    return this.http.delete<any>(
+      `${this.apiUrl}/${id}`,
+      {
+        headers: this.getAuthHeaders()
+      }
+    );
   }
 }
--- frontend/services/series.service.ts
+++ frontend/services/series.service.ts
@@ -1,28 +1,64 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import {
+  HttpClient,
+  HttpHeaders
+} from '@angular/common/http';
+
 import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
+
 export class SeriesService {
+
   private apiUrl = 'http://localhost:3000/api/series';
 
   constructor(private http: HttpClient) {}
 
+  private getAuthHeaders(): HttpHeaders {
+
+    const token = localStorage.getItem('token');
+
+    return new HttpHeaders({
+      Authorization: `Bearer ${token}`
+    });
+  }
+
   getSeries(): Observable<any[]> {
+
     return this.http.get<any[]>(this.apiUrl);
   }
 
   createSerie(serie: any): Observable<any> {
-    return this.http.post<any>(this.apiUrl, serie);
+
+    return this.http.post<any>(
+      this.apiUrl,
+      serie,
+      {
+        headers: this.getAuthHeaders()
+      }
+    );
   }
 
   updateSerie(id: string, serie: any): Observable<any> {
-    return this.http.put<any>(`${this.apiUrl}/${id}`, serie);
+
+    return this.http.put<any>(
+      `${this.apiUrl}/${id}`,
+      serie,
+      {
+        headers: this.getAuthHeaders()
+      }
+    );
   }
 
   deleteSerie(id: string): Observable<any> {
-    return this.http.delete<any>(`${this.apiUrl}/${id}`);
+
+    return this.http.delete<any>(
+      `${this.apiUrl}/${id}`,
+      {
+        headers: this.getAuthHeaders()
+      }
+    );
   }
 }
